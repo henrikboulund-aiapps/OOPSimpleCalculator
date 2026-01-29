45ef4fcc9d7ee0822b5676525198149c692ec02e
Henrik Boulund Meng Hansen <hbh@easv.dk>
2026-01-29 23:10:10 +0100

    Prepared the main.py for the OOP approach.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import CalcEngine
 
-while True:
-    print("===SIMPLE CALCULATOR===")
+def main() -> None:
+    while True:
+        print("===SIMPLE CALCULATOR===")
 
-    print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division")
+        print("1) Addition\n2) Subtraction\n3) Multiplication\n4) Division")
 
-    choice: int = int(input("Enter your choice: "))
+        choice: int = int(input("Enter your choice: "))
 
-    def request_number_inputs() -> list[str]:
-        number1 = int(input("Enter first number: "))
-        number2 = int(input("Enter second number: "))
+        def request_number_inputs() -> list[str]:
+            number1 = int(input("Enter first number: "))
+            number2 = int(input("Enter second number: "))
 
-        list1 = [number1, number2]
+            list1 = [number1, number2]
 
-        return list1
+            return list1
 
-    response : list[str] = request_number_inputs()
-    engine = CalcEngine.CalcEngine(choice, response[0], response[1])
+        response : list[str] = request_number_inputs()
+        engine = CalcEngine.CalcEngine(choice, response[0], response[1])
 
-    print("RESULT: ", engine.calculation())
+        print("RESULT: ", engine.calculation())
+
+if __name__ == "__main__":
+    main()
